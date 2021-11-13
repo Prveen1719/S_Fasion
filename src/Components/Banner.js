f6ddd5c61c7cd90ccdb1998837ce5f8641e355ed
Prveen1719 <71693565+Prveen1719@users.noreply.github.com>
2021-11-13 18:21:03 +0530

    loading page and animation done - moving to second partition

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/styles';
+import { Collapse, IconButton } from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -7,23 +9,43 @@ const useStyles = makeStyles((theme) => ({
         justifyContent: 'center',
         alignItems: 'center',
         height: '100vh',
+        fontFamily: 'Nunito',
+        textAlign: 'center',
         
     },
     colortext: {
         color: '#5AFF3D',
     },
+    title: {
+        color: '#fff',
+        fontSize: '4.5rem',
+    },
+    goDown: {
+        color: '#5AFF3D',
+        fontSize: '4rem',
+    }
 }));
 
 export default function Banner() {
 
     const classes = useStyles();
-
+    const [checked, setChecked] = useState(false);
+    useEffect(() => {
+        setChecked(true);
+    }, []);
     return (
         <div className={classes.root}>
-            <h1>
+
+        {/* using material ui transitions to make animated title */}
+        <Collapse in={checked} {...(checked ? { timeout: 1000 } : {})} collapsedHeight={40}> 
+            <h1 className={classes.title}>
                 Welcome to <br />
                 <span className={classes.colortext}>Fashion-S</span>
             </h1>
+            <IconButton>
+                <ExpandMoreIcon className={classes.goDown} />
+            </IconButton>
+        </Collapse>
         </div>
     )
 }
